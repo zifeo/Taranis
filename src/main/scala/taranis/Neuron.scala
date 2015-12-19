8b8e60f4767bd192e6449435c0acae27d64c120a
Teo Stocco <teo.stocco@epfl.ch>
2015-12-19 16:42:41 +0100

    add simple spikes examples

--- src/main/scala/taranis/Neuron.scala
+++ src/main/scala/taranis/Neuron.scala
@@ -1,20 +1,39 @@
 package taranis
 
-import akka.actor.{Props, ActorLogging, Actor}
+import akka.actor.{ActorRef, Props, ActorLogging, Actor}
+import taranis.Neuron.Register
 import taranis.events.Spike
+
+import scala.collection.mutable
 
 class Neuron extends Actor with ActorLogging {
 
+  private var potential = 0
+  private val successors = mutable.ListBuffer.empty[ActorRef]
+
   override def receive: Receive = {
+
+    case Register(successor) =>
+      log.debug(s"$self register $successor")
+      successors += successor
+
     case Spike(weight) =>
+      log.debug(s"got spike with weight $weight from $sender")
+      potential += 10
+      if (potential > 15) spike(10)
 
   }
+
+  def spike(weight: Int): Unit =
+    successors.foreach(_ ! Spike(weight))
 
 }
 
 object Neuron {
 
   def props: Props =
-    Props(classOf[Neuron])
+    Props(new Neuron)
+
+  final case class Register(successor: ActorRef)
 
 }
--- src/main/scala/taranis/examples/Spikes.scala
+++ src/main/scala/taranis/examples/Spikes.scala
@@ -2,15 +2,23 @@ package taranis.examples
 
 import akka.actor.ActorSystem
 import taranis.Neuron
+import taranis.Neuron.Register
 import taranis.events.Spike
+
+import scala.io.StdIn
 
 object Spikes extends App {
 
   implicit val system = ActorSystem("Taranis")
 
-  val n1 = system.actorOf(Neuron.props)
-  val n2 = system.actorOf(Neuron.props)
+  val n1 = system.actorOf(Neuron.props, "n1")
+  val n2 = system.actorOf(Neuron.props, "n2")
+
+  n1 ! Register(n2)
 
   n1 ! Spike(10)
+  n1 ! Spike(10)
+
+  system.terminate()
 
 }
