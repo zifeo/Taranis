ecea9b234e953dcbd32e93801840538f8da218d0
Teo Stocco <teo.stocco@epfl.ch>
2015-12-24 15:39:38 +0100

    finalize state vs mutable benchmark

--- src/main/scala/taranis/benchmark/StateVsMutable.scala
+++ src/main/scala/taranis/benchmark/StateVsMutable.scala
@@ -4,22 +4,20 @@ import akka.actor.{Actor, ActorSystem, Props}
 import org.scalameter._
 import taranis.benchmark.StateVsMutable.Incr
 
-import scala.concurrent.Promise
+import scala.concurrent.{Await, Promise}
+import scala.concurrent.duration.Duration
 
 class StateIncr(termination: Promise[Unit], threshold: Int) extends Actor {
 
   override def receive: Receive = state(0)
 
   def state(count: Int): Receive = {
-
     case Incr if count > threshold =>
       termination.success(())
     case Incr =>
       context.become(state(count + 1))
       self ! Incr
-
   }
-
 }
 
 class MutableIncr(termination: Promise[Unit], threshold: Int) extends Actor {
@@ -27,24 +25,22 @@ class MutableIncr(termination: Promise[Unit], threshold: Int) extends Actor {
   var count = 0
 
   override def receive: Actor.Receive = {
-
     case Incr if count > threshold =>
       termination.success(())
     case Incr =>
       count += 1
       self ! Incr
   }
-
 }
 
 object StateVsMutable extends App {
 
-  implicit val system = ActorSystem("Taranis")
+  implicit val system = ActorSystem("Taranis-benchmark")
   case object Incr
 
-  val threshold = 100000000
+  val threshold = 100000
   val bench = config(
-    Key.exec.benchRuns -> 10000,
+    Key.exec.benchRuns -> 1000,
     Key.verbose -> true
   ) withWarmer {
     new Warmer.Default
@@ -56,15 +52,22 @@ object StateVsMutable extends App {
     val termination = Promise[Unit]()
     val state = system.actorOf(Props(classOf[StateIncr], termination, threshold))
     state ! Incr
+    Await.result(termination.future, Duration.Inf)
   }
 
   val mutableTime = bench measure {
     val termination = Promise[Unit]()
     val mutable = system.actorOf(Props(classOf[MutableIncr], termination, threshold))
     mutable ! Incr
+    Await.result(termination.future, Duration.Inf)
   }
 
   println(s"state: $stateTime")
   println(s"mutable: $mutableTime")
+
+  // state: 22.638966228000047 ms
+  // mutable: 19.53756763299998 ms
+
+  Await.result(system.terminate(), Duration.Inf)
 
 }
