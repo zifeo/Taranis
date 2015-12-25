3dbd22051b27d0e7559d6c5c100618ec833da403
Teo Stocco <teo.stocco@epfl.ch>
2015-12-25 03:29:31 +0100

    beat some c++ simulator case

---
+++ src/main/scala/taranis/benchmarks/TwoNeuronsBenchmark.scala
@@ -0,0 +1,33 @@
+package taranis.benchmarks
+
+import taranis.dsl._
+import taranis.models.devices._
+import taranis.models.neurons._
+import taranis.models.synapses._
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+object TwoNeuronsBenchmark extends App {
+
+  val run = bench { _ =>
+
+    val weight = 20.0
+    val delay = 1
+    val stim = 1000.0
+
+    val neuron1 = create(IafNeuron.withParams(Ie = stim))
+    val neuron2 = create(IafNeuron.default)
+    val multimeter = create(Multimeter.withRecorders[IafNeuron](
+      "Vm" -> (_.Vm)
+    ))
+
+    connect(neuron1, neuron2, StaticSynapse.withParams(weight = weight, delay = delay))
+    connect(multimeter, neuron2)
+
+    simulate(100 milliseconds, 1 millisecond)
+  }
+
+  println(s"run: $run")
+
+}
